94fa2110c1f7ca1a71036b1701b57575120dcd80
azazmuzaffar <sp18-bcs-035@cuiwah.edu.pk>
2023-01-27 12:52:45 +0500

    Small fixes!

--- js/app.js
+++ js/app.js
@@ -5,28 +5,50 @@ $(document).ready(function () {
   var video = document.querySelector(".hero-banner video");
   var playVideo = $(".hero-banner .play__video");
 
-  function videofunctions() {
-    video.muted = !video.muted;
-    console.log(video);
-    let heroBanner = document.querySelector(".hero-banner .video-wrapper");
-    let currentCursor = heroBanner.style.cursor;
-    if (currentCursor.includes("play")) {
-      heroBanner.style.cursor = "url('./images/icons/pause.svg'), auto";
-    } else {
-      heroBanner.style.cursor = "url('./images/icons/play.svg'), auto";
-    }
-  }
+  video.playbackRate = 0.75;
+
+  window.addEventListener("resize", function () {});
 
   if (video) {
     video.addEventListener("click", function (e) {
-      videofunctions();
+      if ($(window).width() > 992) {
+        video.muted = !video.muted;
+        let heroBanner = document.querySelector(".hero-banner");
+        let currentCursor = heroBanner.style.cursor;
+
+        if (currentCursor.includes("play")) {
+          heroBanner.style.cursor = "url('./images/icons/pause.svg'), auto";
+        } else {
+          heroBanner.style.cursor = "url('./images/icons/play.svg'), auto";
+        }
+
+        if (video.playbackRate == 0.75) {
+          video.playbackRate = 1.0;
+        } else {
+          video.playbackRate = 0.75;
+        }
+      }
     });
   }
 
   if (playVideo) {
     playVideo.click(function () {
       $(this).toggleClass("show");
-      videofunctions();
+      video.muted = !video.muted;
+      let heroBanner = document.querySelector(".hero-banner");
+      let currentCursor = heroBanner.style.cursor;
+
+      if (currentCursor.includes("play")) {
+        heroBanner.style.cursor = "url('./images/icons/pause.svg'), auto";
+      } else {
+        heroBanner.style.cursor = "url('./images/icons/play.svg'), auto";
+      }
+
+      if (video.playbackRate == 0.75) {
+        video.playbackRate = 1.0;
+      } else {
+        video.playbackRate = 0.75;
+      }
     });
   }
 });
